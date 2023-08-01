545dab3b2da0ca55996b7130e6fcfe9586e74978
oliverloza <oliverbree@gmail.com>
2023-07-31 23:18:34 -0700

    completed functions continued

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// code your solution here
+function saturdayFun(activity) {
+    if (activity === undefined) {
+      activity = "roller-skate"; 
+    }
+    return `This Saturday, I want to ${activity}!`;
+  }
+  
+function mondayWork(activity = "go to the office") {
+    return `This Monday, I will ${activity}.`;
+  }
+  function wrapAdjective(flair = "*") {
+    return function (adjective = "special") {
+      return `You are ${flair}${adjective}${flair}!`;
+    };
+  }
+
+  
+  
+  
